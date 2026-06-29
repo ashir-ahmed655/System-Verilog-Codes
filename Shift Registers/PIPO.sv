b100f7f64c3944ea8c9cb40c5b86d4396bac800d
Ashir Ahmed <109139732+ashir-ahmed655@users.noreply.github.com>
2026-06-29 20:27:15 +0500

    Create PIPO.sv

---
+++ Shift Registers/PIPO.sv
@@ -0,0 +1,16 @@
+module pipo (
+    input  logic       clk,
+    input  logic       rst,
+    input  logic       load,
+    input  logic [3:0] parallel_in,
+    output logic [3:0] parallel_out
+);
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst)
+            parallel_out <= 4'b0000;
+        else if (load)
+            parallel_out <= parallel_in;
+    end
+
+endmodule
